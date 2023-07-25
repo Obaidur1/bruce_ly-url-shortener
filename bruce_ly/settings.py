60e8965af961dbd982ab7d8332204e3fb5780e53
Obaidur Rahman <thetutorlab360@gmail.com>
2023-07-25 21:08:19 +0600

    Ready for deploy

--- bruce_ly/settings.py
+++ bruce_ly/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +133,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 COMPRESS_ROOT = BASE_DIR / "static"
 COMPRESS_ENABLED = True
 STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
+# for vercel
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
