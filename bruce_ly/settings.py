a427880fc819d243bc5aaa0ee67d1a3ea98fe471
Obaidur Rahman <thetutorlab360@gmail.com>
2023-07-25 22:56:25 +0600

    Bug removed

--- bruce_ly/settings.py
+++ bruce_ly/settings.py
@@ -133,5 +133,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 COMPRESS_ROOT = BASE_DIR / "static"
 COMPRESS_ENABLED = True
 STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
-# for vercel
-ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
